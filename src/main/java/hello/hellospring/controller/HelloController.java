89aff920bea87b6bb55607c152dc5140e0961c1d
dhkdn9192 <dhkdn9192@naver.com>
2024-01-16 20:54:19 +0900

    first commit

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,40 @@ public class HelloController {
         model.addAttribute("data", "hohoho!!!");
         return "helloooo";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+        // @ResponseBody를 쓰면 return 값을 반환할 때  view를 쓰지 않음
+        // 따라서 웹 브라우저로 확인해도 html 템플릿 없이 위 문자열만 그대로 전달됨
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        // 위에서 ResponseBody가 문자열을 반환하는 경우, 그대로 반환했지만
+        // 객체를 반환할 땐 디폴트로 JSON 구조로 데이터를 만들어 반환하도록 설계됨
+    }
+
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 
 }
